bdcd0269ec7174640ba70bcd540ea2751e51210c
Fernando Oliveira <fernando.oliveira@thomsonreuters.com>
2020-09-18 21:24:41 -0300

    Delete

--- src/main/java/br/com/stoom/challenge/controller/StAddressController.java
+++ src/main/java/br/com/stoom/challenge/controller/StAddressController.java
@@ -1,16 +1,17 @@
 package br.com.stoom.challenge.controller;
 
-import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import br.com.stoom.challenge.entity.StAddress;
+
 import br.com.stoom.challenge.model.StAddressModel;
 import br.com.stoom.challenge.service.StAddressService;
 
@@ -21,13 +22,33 @@ public class StAddressController {
 	@Autowired
 	StAddressService stAddressService;
 
+	@PostMapping
+	public ResponseEntity<StAddressModel> createAddress(@RequestBody StAddressModel stAddressModel) {
+		return stAddressService.createAddress(stAddressModel);
+	}
+
 	@GetMapping
 	public ResponseEntity<?> getAllAdddress() {
 		return ResponseEntity.ok(stAddressService.getAll());
 	}
 
-	@PostMapping
-	public ResponseEntity<StAddressModel> createAddress(@RequestBody StAddressModel stAddressModel) {
-		return stAddressService.createAddress(stAddressModel);
+	@GetMapping("/{id}")
+	public ResponseEntity<StAddressModel> getAddressById(@PathVariable("id") String id) {
+		return ResponseEntity.ok(stAddressService.findById(UUID.fromString(id)).fromEntityToModel());
+	}
+
+	/*
+	 * @PutMapping("/{id}") public ResponseEntity<StAddressModel>
+	 * updateAddress(@PathVariable("id") String id,
+	 * 
+	 * @RequestBody StAddressModel addressModel) { StAddress stAddress =
+	 * fromModel(addressModel).toBuilder().id(UUID.fromString(id)).build(); return
+	 * ResponseEntity.ok(addressService.update(address).toModel()); }
+	 */
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deleteAddressById(@PathVariable("id") String id) {
+		stAddressService.delete(UUID.fromString(id));
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/br/com/stoom/challenge/service/StAddressService.java
+++ src/main/java/br/com/stoom/challenge/service/StAddressService.java
@@ -1,15 +1,13 @@
 package br.com.stoom.challenge.service;
 
-import java.net.URI;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import br.com.stoom.challenge.entity.StAddress;
@@ -22,16 +20,20 @@ public class StAddressService {
 	@Autowired
 	StAddressRepository stAddressRepository;
 
-	@GetMapping
+	public ResponseEntity<StAddressModel> createAddress(@RequestBody StAddressModel stAddressModel) {
+		StAddress stAddress = stAddressRepository.save(new StAddress().fromModelToEntity(stAddressModel));
+		return new ResponseEntity<>(stAddress.fromEntityToModel(), HttpStatus.CREATED);
+	}
+
 	public List<StAddress> getAll() {
 		return stAddressRepository.findAll();
 	}
 
-	@PostMapping
-	public ResponseEntity<StAddressModel> createAddress(@RequestBody StAddressModel stAddressModel) {
+	public StAddress findById(UUID id) {
+		return stAddressRepository.findById(id).orElseThrow(IllegalArgumentException::new);
+	}
 
-		StAddress stAddress = stAddressRepository.save(new StAddress().fromModelToEntity(stAddressModel));
-
-		return new ResponseEntity<>(stAddress.fromEntityToModel(), HttpStatus.CREATED);
+	public void delete(UUID id) {
+		stAddressRepository.deleteById(id);
 	}
 }
